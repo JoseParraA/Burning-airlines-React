2ea2e411727a68b32c074546bb5f799dfac8337a
Sam Cochrane <samcochrane@Sams-MacBook-Air.local>
2018-04-17 23:10:49 +1000

    routes done

---
+++ src/components/Flights.js
@@ -0,0 +1,16 @@
+import React, { PureComponent as Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Flights extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is the Flights page</h2>
+        <Link to="/">Back to home</Link>
+        <Link to="/Reservations">Reservations pages</Link>
+      </div>
+    )
+  }
+}
+
+export default Flights;
