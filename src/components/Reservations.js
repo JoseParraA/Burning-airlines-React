d0350b084023689f924fb25e47be5c8fd7847d30
Sam Cochrane <samcochrane@Sams-MacBook-Air.local>
2018-04-18 15:01:46 +1000

    seating one seat

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,82 +1,102 @@
 import React, { PureComponent as Component } from 'react';
-import { Link } from 'react-router-dom';
 
 
 
-class Reservations extends Component {
-  render() {
-    return (
-      <div>
-        <h2>This is the Reservations page</h2>
-        <Link to="/">Back to home</Link>
-        <Link to="/Flights">Flights page</Link>
-        <PlaneSeats />
-        <SeatsPlan />
-      </div>
-    );
-  }
-}
-
-
-class PlaneSeats extends Component {
-  constructor() {
-    super();
-    this.state = { value: "__" }
-  }
-  render() {
-    return (
-      <button className="book seat" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
-class SeatsPlan extends Component {
-  renderSquare(i) {
-    return <PlaneSeats />;
+class Seat extends Component {
+  checkIfTaken = () => {
+    if ( this.props.takenSeats.indexOf( this.props.seatId ) !== -1 ) { // if this seat is taken
+      this.setState({
+        isTaken: true
+      });
+    } else {
+      this.setState({
+        isTaken: false
+      });
     }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTaken: false,
+      // isSelected: false
+    };
+  }
+
+  componentDidUpdate() {
+    this.checkIfTaken();
+  }
+
+  _handleClick = (e) => {
+    // console.log( this.props.seatId );
+    this.props.getSelectedSeat( this.props.seatId );
+    this.setState({
+      isSelected: true
+    });
+
+  }
 
   render() {
-    const status = '';
-
+    // console.log( this.props.selectedSeat );
     return (
-      <div>
-        <p> Please Check where you would like to sit</p>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
-          {this.renderSquare(15)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(16)}
-          {this.renderSquare(17)}
-          {this.renderSquare(18)}
-          {this.renderSquare(19)}
+      <div className={ this.state.isTaken ? "seat taken" : "seat free" } onClick={ this._handleClick } >
+        <div className={ this.props.selectedSeat === this.props.seatId && !this.state.isTaken ? "selected" : null } >
+
         </div>
       </div>
     );
   }
 }
 
-export default Reservations;
+
+
+
+
+
+class SeatMap extends Component {
+  getSelectedSeat = (s) => {
+    this.setState({
+      selectedSeat: s
+    });
+    this.props.passSeat(s);
+  }
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedSeat: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Seat Map</h2>
+        <p>Please choose an available seat</p>
+        <div className="grid-container" >
+          {/* make a row for number of rows */}
+          { [...Array(this.props.rows)].map((e, i) =>
+            <div className="grid-row" key={i}>
+              {/* row letter */}
+              <span className="row-num">{ String.fromCharCode(i+65) }</span>
+              {/*  make seat re num of cols */}
+              { [...Array(this.props.cols)].map((e, j) =>
+                <Seat
+                  key={`${String.fromCharCode(i+65)}${j+1}`}
+                  seatId={`${String.fromCharCode(i+65)}${j+1}`}
+                  takenSeats={ this.props.takenSeats }
+                  getSelectedSeat={ this.getSelectedSeat }
+                  selectedSeat={ this.state.selectedSeat }
+                />
+              ) }
+            </div>
+          ) }
+        </div>
+        <p>Selected Seat: { this.state.selectedSeat }</p>
+      </div>
+    );
+  }
+}
+
+export default SeatMap;
